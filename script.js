152a742fed2aa96a273f9115c7ed1aa2f8965146
phoenixlly <phoenixdeimos5@gmail.com>
2024-06-11 08:34:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -52,10 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function toggleSection(sectionId) {
     const sections = document.querySelectorAll('.content-section');
     sections.forEach(section => {
-        if (section.id === sectionId) {
-            section.style.display = section.style.display === 'block' ? 'none' : 'block';
-        } else {
-            section.style.display = 'none';
-        }
+        section.style.display = section.id === sectionId ? 'block' : 'none';
     });
 }
